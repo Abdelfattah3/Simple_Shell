344ad633d6a17b6bebd2d719b37ce012cf185369
AlaaMahmoud112003 <full.stack.developer112003@gmail.com>
2023-10-15 09:45:56 -0700

    free list

---
+++ free_list.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* free_list - frees the memory after usage
+* @av :  pointer to a double pointer to the arguments
+*/
+void free_list(char ***av)
+{
+	int d = 0;
+
+	while ((*av)[d] != NULL)
+	{
+		free((*av)[d]);
+		d++;
+	}
+	free((*av));
+}
